6dcfa07d0ceb4fa2949df382ad81dc27cfb1e3a4
João Gabriel de Sá Santana <joao.santana09@outlook.com>
2023-02-22 14:00:32 -0300

    finish module 01 fundamentos

--- src/server.js
+++ src/server.js
@@ -1,33 +1,27 @@
 import http from "node:http";
-import { randomUUID } from "node:crypto";
 
-import { Database } from "./database.js";
 import { json } from "./middlewares/json.js";
+import { routes } from "./routes.js";
+import { extractQueryParams } from "./utils/extract-query-params.js";
 
-const db = new Database();
 
 const server = http.createServer(async (req, res) => {
   const { method, url } = req;
 
   await json(req, res);
 
-  if (method === "GET" && url === "/users") {
-    return res.end(JSON.stringify(db.select("users")));
+  const route = routes.find(route => route.method === method && route.path.test(url));
+
+  if (route) {
+    const routeParams = req.url.match(route.path);
+    const { query, ...params } = routeParams.groups;
+
+    req.params = params;
+    req.query = query ? extractQueryParams(query) : {};
+
+    return route.handler(req, res);
   }
 
-  if (method === "POST" && url === "/users") {
-    const { name, email } = req.body;
-
-    const user = {
-      id: randomUUID(),
-      name,
-      email,
-    };
-
-    db.insert("users", user);
-
-    return res.writeHead(201).end();
-  }
 
   res.writeHead(404).end();
 });
